db7ffce6237ab4fa90446fbbcd01e53c3bc44126
Maksim Korenev <subzone@list.ru>
2020-11-19 23:30:04 +0800

    Updates

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,36 @@
 <template>
-  <h1>Hello!</h1>
+  <div>
+    <div
+      v-for="home in homes"
+      :key="home.objectID"
+      style="float: left; margin: 10px"
+    >
+      <nuxt-link :to="`/home/${home.objectID}`">
+        <home-card :home="home" />
+      </nuxt-link>
+    </div>
+  </div>
 </template>
+
+<script>
+import homes from "~/data/homes.json";
+export default {
+  head() {
+    return {
+      title: "Homepage",
+      meta: [
+        {
+          name: "description",
+          content: "This is a homepage!",
+          hid: "description",
+        },
+      ],
+    };
+  },
+  data() {
+    return {
+      homes: homes.slice(0, 3),
+    };
+  },
+};
+</script>
